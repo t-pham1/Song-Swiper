62db831c676e65a17b656a9d5ede82e06ced9f16
Tommy Pham <tommypham1@yahoo.com>
2025-05-01 20:44:56 -0500

    refactored routes, removed get_all_tracks helper function

--- app.py
+++ app.py
@@ -25,82 +25,67 @@ sp_oauth = SpotifyOAuth(
     redirect_uri = REDIRECT_URI,
     scope = SCOPE,
     cache_handler = cache_handler,
-    # show_dialog = True # true for debugging purposes, makes user authenticate every hour?
+    # show_dialog = True # true for debugging purposes, makes user authenticate with each login
 )
 
 # --- helper functions ---
-def require_spotify_auth(f):
+def require_spotify_auth(f): # if token doesn't exist, use authorization to get one
     @wraps(f)
     def wrapper(*args, **kwargs):
         token = sp_oauth.get_cached_token()
-        if not token or not sp_oauth.validate_token(token):
+        if not token:
             return redirect(sp_oauth.get_authorize_url())
         return f(*args, **kwargs)
     return wrapper
 
-def get_spotify_client():
+def get_spotify_client(): # if token doesn't exist or if it's invalid/expired, grant a refresh token. then return spotify client
     token = sp_oauth.get_cached_token()
     if not token or not sp_oauth.validate_token(token):
         token = sp_oauth.refresh_access_token(token['refresh_token'])
     return Spotify(auth=token['access_token']) if token else None
 
-def get_all_tracks(fetch_func, **kwargs):
-    items = []
-    limit = kwargs.get('limit', 50)
-    offset = 0
-
-    while True:
-        page = fetch_func(limit=limit, offset=offset, **kwargs)
-        page_items = page['items']
-        if not page_items:
-            break
-        items.extend(page_items)
-        offset += limit
-
-    return items
-
 # --- routes ---
 @app.route('/')
-def index():
+def index(): # welcome page
     return """
     <h1>Welcome to Song Swiper!</h1>
     <a href='/login'><button>Login</button></a>
     """
 
-@app.route('/login')
+@app.route('/login') # authorization page to get access token
 def login():
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
 
-@app.route('/callback')
+@app.route('/callback') # default to dashboard route
 def callback():
     sp_oauth.get_access_token(request.args['code'])
     return redirect(url_for('dashboard'))
 
-@app.route('/dashboard')
+@app.route('/dashboard') # main home page, display username
 @require_spotify_auth
 def dashboard():
     sp = get_spotify_client()
     user = sp.current_user()
-    name = user.get('display_name', 'user')
+    username = user['display_name']
     
     dashboard_html = f"""
-    <h1>Welcome, {name}!</h1>
+    <h1>Welcome, {username}!</h1>
     <a href='/select_playlist'><button>Select playlist</button></a>
     <br><a href='/logout'><button>Logout</button></a>
     """
     
     return dashboard_html
 
-@app.route('/select_playlist')
+@app.route('/select_playlist') # select playlist or liked songs
 @require_spotify_auth
 def select_playlist():
     sp = get_spotify_client()
     # user = sp.current_user()
     playlists = sp.current_user_playlists()
-    form_html = "<h1>Select a playlist</h1><form method='POST' action='/selected_playlist'>"
+    form_html = "<h1>Select a Playlist</h1><form method='POST' action='/selected_playlist'>"
     
-    form_html += "<input type='radio' name='playlist_id' value='liked' required> Liked Songs<br>"
+    form_html += "<input type='radio' name='playlist_id' value='liked_songs' required> Liked Songs<br>"
     
     for playlist in playlists['items']:
         playlist_id = playlist['id']
@@ -112,29 +97,28 @@ def select_playlist():
     
     return form_html
 
-@app.route('/selected_playlist', methods=['POST'])
+@app.route('/selected_playlist', methods=['POST']) # display track info in selected playlist (tabular), paginate to 50 results
 @require_spotify_auth
 def selected_playlist():
     sp = get_spotify_client()
     # user = sp.current_user()
     playlist_id = request.form['playlist_id']
     
-    if playlist_id == 'liked':
-        playlist = sp.current_user_saved_tracks()
+    if playlist_id == 'liked_songs':
         playlist_name = 'Liked Songs'
-        tracks = get_all_tracks(sp.current_user_saved_tracks)
+        tracks = sp.current_user_saved_tracks()
     else:
         playlist = sp.playlist(playlist_id)
         playlist_name = playlist['name']
-        tracks = get_all_tracks(sp.playlist_items, playlist_id=playlist_id)
+        tracks = sp.playlist_tracks(playlist_id)
     
     track_list = "<ol style='line-height: 1.8;'>"
-    for item in tracks:
+    for item in tracks['items']:
         track = item['track']
         track_name = track['name']
         album_name = track['album']['name']
         artist_names = ', '.join([artist['name'] for artist in track['artists']])
-        track_list += f"<li>{track_name}  |  {album_name}  |  {artist_names}</li>"
+        track_list += f"<li>{track_name}\t|\t{album_name}\t|\t{artist_names}</li>"
     track_list += "</ol>"
     
     return f"""
@@ -144,7 +128,7 @@ def selected_playlist():
     <br><a href='/dashboard'><button>Back to dashboard</button></a>
     """
 
-@app.route('/logout')
+@app.route('/logout') # clear session and redirect to index route
 def logout():
     session.clear()
     return redirect(url_for('index'))
